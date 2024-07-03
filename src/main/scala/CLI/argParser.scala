5f864e2b45e10966dfff901c2427a71b150fb772
Elliot <elliotadams97@yahoo.co.uk>
2024-07-03 19:16:16 +0100

    Modified argParser so generic Seq can be used

--- src/main/scala/CLI/argParser.scala
+++ src/main/scala/CLI/argParser.scala
@@ -2,27 +2,27 @@ package CLI
 
 import Domain._
 import CLI.CLIStrings._
-
+import CLI.CLIArg.getCLIArg
 
 object argParser {
 
-  def parseArgs(arrOfArgs: Array[String]): Either[Error,List[Option[CLIArg]]]= {
-    val listArgs = checkForNoArgs(arrOfArgs.toList)
+  def parseArgs(arrOfArgs: Array[String]): Either[Error,Seq[CLIArg]]= {
+    val listArgs = checkForNoArgs(arrOfArgs.toVector)
     val helpcheck = listArgs.flatMap(checkForHelpArg)
   }
 
 
-  def checkForNoArgs(listOfArgs: List[String]): Either[NoCLIArgError.type,List[String]] =
-    if (listOfArgs.isEmpty) Left(NoCLIArgError)
-    else Right(listOfArgs)
+  def checkForNoArgs[A](args: Seq[A]): Either[NoCLIArgError.type,Seq[A]] =
+    if (args.isEmpty) Left(NoCLIArgError)
+    else Right(args)
 
-  def checkForHelpArg(listOfArgs: List[String]): Either[HelpArgError,List[String]] = {
-    val helpArgsWithIndex = listOfArgs.zipWithIndex.filter(_._1 == "--help")
+  def checkForHelpArg[A](args: Seq[String]): Either[HelpArgError,Seq[String]] = {
+    val helpArgsWithIndex = args.zipWithIndex.filter(_._1 == "--help")
 
     helpArgsWithIndex match {
       case _ if helpArgsWithIndex.length > 1 => Left(HelpArgError(TOO_MANY_HELP))
       case _ if helpArgsWithIndex.head._2 != 0 => Left(HelpArgError(BAD_HELP))
-      case _ if helpArgsWithIndex.isEmpty => Right(listOfArgs)
+      case _ if helpArgsWithIndex.isEmpty => Right(args)
       case _ => Left(HelpArgError(HELP_STRING))
     }
   }
