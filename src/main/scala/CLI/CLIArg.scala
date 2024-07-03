64b4a4b481a565d4a5556b8f214dd7fda7bf07bc
Elliot <elliotadams97@yahoo.co.uk>
2024-07-03 19:08:35 +0100

    Added getCLIArg method

--- src/main/scala/CLI/CLIArg.scala
+++ src/main/scala/CLI/CLIArg.scala
@@ -1,19 +1,28 @@
 package CLI
 
+import scala.annotation.tailrec
+import Domain.MandatoryCLIArgsNotPresentError
+
 sealed trait CLIArg
 sealed trait mandatoryCLIArg extends CLIArg
 sealed trait optionalCLIArg extends CLIArg
 case object helpArg extends optionalCLIArg
 final case class unknownArg(unknownVal: String) extends CLIArg
 final case class configArg(configPath: String) extends optionalCLIArg
-final case class modeArg(mode: Int) extends mandatoryCLIArg
+final case class modeArg(mode: String) extends mandatoryCLIArg
 
 object CLIArg {
-  def getCLIArg(s: String): CLIArg = s match {
-    case "--help" => helpArg
-    case "-c" => ???
-    case "--config" => ???
-    case "-m" => ???
-    case "--mode" => ???
+  @tailrec
+  def getCLIArg(args: Seq[String], CLIArgs: Seq[CLIArg] = Seq[CLIArg]): Seq[CLIArg] = {
+
+    if (args.isEmpty) CLIArgs
+    else {
+      args.head match {
+        case "-c" | "--config" => getCLIArg(args.drop(2), CLIArgs :+ configArg(args.tail.head))
+        case "-m" | "--mode" => getCLIArg(args.drop(2), CLIArgs :+ modeArg(args.tail.head))
+        case unknownVal => getCLIArg(args.tail, CLIArgs :+ unknownArg(args.head))
+      }
+    }
+  }
   }
 }
